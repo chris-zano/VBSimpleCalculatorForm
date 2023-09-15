f6e4b29a69d0506983327bfe34caca81d71d0b70
Christian Solomon <cncs.chris@gmail.com>
2023-09-15 09:07:14 +0000

    initialise repository

---
+++ Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtDisplay = New System.Windows.Forms.TextBox()
+        Me.btn_clearall = New System.Windows.Forms.Button()
+        Me.btn_clear = New System.Windows.Forms.Button()
+        Me.btn_del = New System.Windows.Forms.Button()
+        Me.btn_mul = New System.Windows.Forms.Button()
+        Me.btn_7 = New System.Windows.Forms.Button()
+        Me.btn_8 = New System.Windows.Forms.Button()
+        Me.btn_9 = New System.Windows.Forms.Button()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.btn_4 = New System.Windows.Forms.Button()
+        Me.btn_5 = New System.Windows.Forms.Button()
+        Me.btn_6 = New System.Windows.Forms.Button()
+        Me.btn_div = New System.Windows.Forms.Button()
+        Me.btn_1 = New System.Windows.Forms.Button()
+        Me.btn_2 = New System.Windows.Forms.Button()
+        Me.btn_3 = New System.Windows.Forms.Button()
+        Me.btn_0 = New System.Windows.Forms.Button()
+        Me.btn_point = New System.Windows.Forms.Button()
+        Me.btn_equalto = New System.Windows.Forms.Button()
+        Me.btn_sub = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtDisplay
+        '
+        Me.TxtDisplay.Location = New System.Drawing.Point(22, 26)
+        Me.TxtDisplay.Multiline = True
+        Me.TxtDisplay.Name = "TxtDisplay"
+        Me.TxtDisplay.Size = New System.Drawing.Size(186, 33)
+        Me.TxtDisplay.TabIndex = 0
+        '
+        'btn_clearall
+        '
+        Me.btn_clearall.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.btn_clearall.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_clearall.Location = New System.Drawing.Point(185, 76)
+        Me.btn_clearall.Name = "btn_clearall"
+        Me.btn_clearall.Size = New System.Drawing.Size(75, 62)
+        Me.btn_clearall.TabIndex = 1
+        Me.btn_clearall.Text = "CE"
+        Me.btn_clearall.UseVisualStyleBackColor = False
+        '
+        'btn_clear
+        '
+        Me.btn_clear.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.btn_clear.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_clear.Location = New System.Drawing.Point(98, 76)
+        Me.btn_clear.Name = "btn_clear"
+        Me.btn_clear.Size = New System.Drawing.Size(75, 62)
+        Me.btn_clear.TabIndex = 2
+        Me.btn_clear.Text = "C"
+        Me.btn_clear.UseVisualStyleBackColor = False
+        '
+        'btn_del
+        '
+        Me.btn_del.BackColor = System.Drawing.Color.Red
+        Me.btn_del.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_del.Location = New System.Drawing.Point(12, 76)
+        Me.btn_del.Name = "btn_del"
+        Me.btn_del.Size = New System.Drawing.Size(75, 62)
+        Me.btn_del.TabIndex = 3
+        Me.btn_del.Text = "DEL"
+        Me.btn_del.UseVisualStyleBackColor = False
+        '
+        'btn_mul
+        '
+        Me.btn_mul.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btn_mul.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_mul.Location = New System.Drawing.Point(273, 162)
+        Me.btn_mul.Name = "btn_mul"
+        Me.btn_mul.Size = New System.Drawing.Size(75, 59)
+        Me.btn_mul.TabIndex = 4
+        Me.btn_mul.Text = "*"
+        Me.btn_mul.UseVisualStyleBackColor = False
+        '
+        'btn_7
+        '
+        Me.btn_7.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_7.Location = New System.Drawing.Point(185, 162)
+        Me.btn_7.Name = "btn_7"
+        Me.btn_7.Size = New System.Drawing.Size(75, 59)
+        Me.btn_7.TabIndex = 5
+        Me.btn_7.Text = "7"
+        Me.btn_7.UseVisualStyleBackColor = True
+        '
+        'btn_8
+        '
+        Me.btn_8.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_8.Location = New System.Drawing.Point(98, 162)
+        Me.btn_8.Name = "btn_8"
+        Me.btn_8.Size = New System.Drawing.Size(75, 59)
+        Me.btn_8.TabIndex = 6
+        Me.btn_8.Text = "8"
+        Me.btn_8.UseVisualStyleBackColor = True
+        '
+        'btn_9
+        '
+        Me.btn_9.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_9.Location = New System.Drawing.Point(12, 162)
+        Me.btn_9.Name = "btn_9"
+        Me.btn_9.Size = New System.Drawing.Size(75, 59)
+        Me.btn_9.TabIndex = 7
+        Me.btn_9.Text = "9"
+        Me.btn_9.UseVisualStyleBackColor = True
+        '
+        'btn_add
+        '
+        Me.btn_add.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btn_add.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_add.Location = New System.Drawing.Point(273, 242)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(75, 63)
+        Me.btn_add.TabIndex = 8
+        Me.btn_add.Text = "+"
+        Me.btn_add.UseVisualStyleBackColor = False
+        '
+        'btn_4
+        '
+        Me.btn_4.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_4.Location = New System.Drawing.Point(185, 242)
+        Me.btn_4.Name = "btn_4"
+        Me.btn_4.Size = New System.Drawing.Size(75, 63)
+        Me.btn_4.TabIndex = 9
+        Me.btn_4.Text = "4"
+        Me.btn_4.UseVisualStyleBackColor = True
+        '
+        'btn_5
+        '
+        Me.btn_5.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_5.Location = New System.Drawing.Point(98, 242)
+        Me.btn_5.Name = "btn_5"
+        Me.btn_5.Size = New System.Drawing.Size(75, 63)
+        Me.btn_5.TabIndex = 10
+        Me.btn_5.Text = "5"
+        Me.btn_5.UseVisualStyleBackColor = True
+        '
+        'btn_6
+        '
+        Me.btn_6.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_6.Location = New System.Drawing.Point(12, 242)
+        Me.btn_6.Name = "btn_6"
+        Me.btn_6.Size = New System.Drawing.Size(75, 63)
+        Me.btn_6.TabIndex = 11
+        Me.btn_6.Text = "6"
+        Me.btn_6.UseVisualStyleBackColor = True
+        '
+        'btn_div
+        '
+        Me.btn_div.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btn_div.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_div.Location = New System.Drawing.Point(273, 320)
+        Me.btn_div.Name = "btn_div"
+        Me.btn_div.Size = New System.Drawing.Size(75, 57)
+        Me.btn_div.TabIndex = 12
+        Me.btn_div.Text = "/"
+        Me.btn_div.UseVisualStyleBackColor = False
+        '
+        'btn_1
+        '
+        Me.btn_1.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_1.Location = New System.Drawing.Point(185, 320)
+        Me.btn_1.Name = "btn_1"
+        Me.btn_1.Size = New System.Drawing.Size(75, 57)
+        Me.btn_1.TabIndex = 13
+        Me.btn_1.Text = "1"
+        Me.btn_1.UseVisualStyleBackColor = True
+        '
+        'btn_2
+        '
+        Me.btn_2.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_2.Location = New System.Drawing.Point(98, 320)
+        Me.btn_2.Name = "btn_2"
+        Me.btn_2.Size = New System.Drawing.Size(75, 57)
+        Me.btn_2.TabIndex = 14
+        Me.btn_2.Text = "2"
+        Me.btn_2.UseVisualStyleBackColor = True
+        '
+        'btn_3
+        '
+        Me.btn_3.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_3.Location = New System.Drawing.Point(12, 320)
+        Me.btn_3.Name = "btn_3"
+        Me.btn_3.Size = New System.Drawing.Size(75, 57)
+        Me.btn_3.TabIndex = 15
+        Me.btn_3.Text = "3"
+        Me.btn_3.UseVisualStyleBackColor = True
+        '
+        'btn_0
+        '
+        Me.btn_0.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_0.Location = New System.Drawing.Point(12, 394)
+        Me.btn_0.Name = "btn_0"
+        Me.btn_0.Size = New System.Drawing.Size(75, 59)
+        Me.btn_0.TabIndex = 16
+        Me.btn_0.Text = "0"
+        Me.btn_0.UseVisualStyleBackColor = True
+        '
+        'btn_point
+        '
+        Me.btn_point.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btn_point.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_point.Location = New System.Drawing.Point(98, 394)
+        Me.btn_point.Name = "btn_point"
+        Me.btn_point.Size = New System.Drawing.Size(75, 59)
+        Me.btn_point.TabIndex = 17
+        Me.btn_point.Text = "."
+        Me.btn_point.UseVisualStyleBackColor = False
+        '
+        'btn_equalto
+        '
+        Me.btn_equalto.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.btn_equalto.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_equalto.Location = New System.Drawing.Point(185, 394)
+        Me.btn_equalto.Name = "btn_equalto"
+        Me.btn_equalto.Size = New System.Drawing.Size(75, 59)
+        Me.btn_equalto.TabIndex = 18
+        Me.btn_equalto.Text = "="
+        Me.btn_equalto.UseVisualStyleBackColor = False
+        '
+        'btn_sub
+        '
+        Me.btn_sub.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btn_sub.Font = New System.Drawing.Font("CodeNewRoman Nerd Font", 12.0!)
+        Me.btn_sub.Location = New System.Drawing.Point(273, 394)
+        Me.btn_sub.Name = "btn_sub"
+        Me.btn_sub.Size = New System.Drawing.Size(75, 59)
+        Me.btn_sub.TabIndex = 19
+        Me.btn_sub.Text = "-"
+        Me.btn_sub.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(358, 492)
+        Me.Controls.Add(Me.btn_sub)
+        Me.Controls.Add(Me.btn_equalto)
+        Me.Controls.Add(Me.btn_point)
+        Me.Controls.Add(Me.btn_0)
+        Me.Controls.Add(Me.btn_3)
+        Me.Controls.Add(Me.btn_2)
+        Me.Controls.Add(Me.btn_1)
+        Me.Controls.Add(Me.btn_div)
+        Me.Controls.Add(Me.btn_6)
+        Me.Controls.Add(Me.btn_5)
+        Me.Controls.Add(Me.btn_4)
+        Me.Controls.Add(Me.btn_add)
+        Me.Controls.Add(Me.btn_9)
+        Me.Controls.Add(Me.btn_8)
+        Me.Controls.Add(Me.btn_7)
+        Me.Controls.Add(Me.btn_mul)
+        Me.Controls.Add(Me.btn_del)
+        Me.Controls.Add(Me.btn_clear)
+        Me.Controls.Add(Me.btn_clearall)
+        Me.Controls.Add(Me.TxtDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtDisplay As TextBox
+    Friend WithEvents btn_clearall As Button
+    Friend WithEvents btn_clear As Button
+    Friend WithEvents btn_del As Button
+    Friend WithEvents btn_mul As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents btn_add As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_div As Button
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_0 As Button
+    Friend WithEvents btn_point As Button
+    Friend WithEvents btn_equalto As Button
+    Friend WithEvents btn_sub As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Dim firstValue, secondValue, result As Integer
+    Dim op As String
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        TxtDisplay.Text = ""
+    End Sub
+
+    Private Sub Number_Click(sender As Object, e As EventArgs) Handles btn_point.Click, btn_9.Click, btn_8.Click, btn_7.Click, btn_6.Click, btn_5.Click, btn_4.Click, btn_3.Click, btn_2.Click, btn_1.Click, btn_0.Click
+        Dim b As Button = sender
+
+        If (TxtDisplay.Text = "0") Then
+            TxtDisplay.Text = ""
+            TxtDisplay.Text = b.Text
+
+        ElseIf (b.Text = ".") Then
+            If (Not TxtDisplay.Text.Contains(".")) Then
+                TxtDisplay.Text = TxtDisplay.Text + b.Text
+            End If
+
+        Else
+            TxtDisplay.Text = TxtDisplay.Text + b.Text
+        End If
+    End Sub
+
+    Private Sub btn_equalto_Click(sender As Object, e As EventArgs) Handles btn_equalto.Click
+        If TxtDisplay.Text = "" Then
+            secondValue = 0
+        Else
+            secondValue = CInt(TxtDisplay.Text)
+        End If
+
+        If op = "+" Then
+            result = firstValue + secondValue
+            TxtDisplay.Text = result
+        ElseIf op = "-" Then
+            result = firstValue - secondValue
+            TxtDisplay.Text = result
+        ElseIf op = "*" Then
+            result = firstValue * secondValue
+            TxtDisplay.Text = result
+        ElseIf op = "/" Then
+            result = firstValue / secondValue
+            TxtDisplay.Text = result
+        End If
+        firstValue = result
+        secondValue = 0
+        result = 0
+    End Sub
+
+    Private Sub btn_clearall_Click(sender As Object, e As EventArgs) Handles btn_clearall.Click
+        TxtDisplay.Text = "0"
+        firstValue = 0
+        secondValue = 0
+        result = 0
+        op = ""
+    End Sub
+
+    Private Sub delBtn(sender As Object, e As EventArgs) Handles btn_del.Click
+        If TxtDisplay.Text.Length > 0 Then
+            TxtDisplay.Text = TxtDisplay.Text.Remove(TxtDisplay.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    Private Sub operator_click(sender As Object, e As EventArgs) Handles btn_sub.Click, btn_mul.Click, btn_div.Click, btn_add.Click
+        Dim b As Button = DirectCast(sender, Button)
+        firstValue = TxtDisplay.Text
+        op = b.Text
+        TxtDisplay.Text = ""
+    End Sub
+End Class
